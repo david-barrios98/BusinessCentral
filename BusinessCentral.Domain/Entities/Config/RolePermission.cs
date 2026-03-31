25fe57ec9f055bd145692b1f5c79134f91c17a67
DAVIDBR <ingeniero.senior28@interrapidisimo.com>
2026-03-31 12:26:40 -0500

    feature: se crea entidad de persomisos roles

---
+++ BusinessCentral.Domain/Entities/Config/RolePermission.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessCentral.Domain.Entities.Config
+{
+    [Table("role_permissions", Schema = "config")]
+    public class RolePermission
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("role_id")]
+        public int RoleId { get; set; }
+
+        [ForeignKey("RoleId")]
+        public virtual Role Role { get; set; } = null!;
+
+        [Required]
+        [Column("permission_id")]
+        public int PermissionId { get; set; }
+
+        [ForeignKey("PermissionId")]
+        public virtual Permission Permission { get; set; } = null!;
+
+        [Column("is_granted")]
+        public bool IsGranted { get; set; } = true;
+    }
+}
