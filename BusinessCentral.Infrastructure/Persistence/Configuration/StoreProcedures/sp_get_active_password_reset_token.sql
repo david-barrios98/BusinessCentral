008a9f229bb77b606bbb98c2bc80d798fdbbcd54
DAVIDBR <ingeniero.senior28@interrapidisimo.com>
2026-04-14 19:01:09 -0500

    refactor: se actualiza sp de consulta token activo

--- BusinessCentral.Infrastructure/Persistence/Configuration/StoreProcedures/sp_get_active_password_reset_token.sql
+++ BusinessCentral.Infrastructure/Persistence/Configuration/StoreProcedures/sp_get_active_password_reset_token.sql
@@ -1,17 +1,33 @@
 CREATE OR ALTER PROCEDURE [auth].[sp_get_active_password_reset_token]
 (
-    @Token VARCHAR(255)
+    @Token VARCHAR(255) = NULL,
+    @ExpiresAt DATETIME2,
+    @UserId INT = NULL
 )
 AS
 BEGIN
     SET NOCOUNT ON;
 
+    IF @UserId IS NOT NULL AND @UserId > 0 AND @Token IS NULL
+BEGIN
 SELECT pr.Id, pr.UserId, pr.Token, pr.ExpiresAt, pr.CreatedAt, pr.UsedAt,
-       ui.Id AS UserId, ui.UserName, ui.Email, ui.FirstName, ui.LastName, ui.CompanyId
+       ui.Id AS UserId, ui.Email as UserName, ui.Email, ui.FirstName, ui.LastName, ui.CompanyId
+FROM [audit].[PasswordResetToken] pr WITH (NOLOCK)
+    INNER JOIN [auth].[UsersInfo] ui WITH (NOLOCK) ON pr.UserId = ui.Id
+WHERE pr.UserId = @UserId
+  AND pr.UsedAt IS NULL
+  AND pr.ExpiresAt > @ExpiresAt;
+END
+ELSE
+BEGIN
+SELECT pr.Id, pr.UserId, pr.Token, pr.ExpiresAt, pr.CreatedAt, pr.UsedAt,
+       ui.Id AS UserId, ui.Email as UserName, ui.Email, ui.FirstName, ui.LastName, ui.CompanyId
 FROM [audit].[PasswordResetToken] pr WITH (NOLOCK)
     INNER JOIN [auth].[UsersInfo] ui WITH (NOLOCK) ON pr.UserId = ui.Id
 WHERE pr.Token = @Token
   AND pr.UsedAt IS NULL
-  AND pr.ExpiresAt > SYSUTCDATETIME();
+  AND pr.ExpiresAt > @ExpiresAt;
+END
+
 END
 GO
